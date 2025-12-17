efacba1df16555ea20caa6c5f1f9427193426345
Kokeh <kokehinc@gmail.com>
2025-12-17 18:21:51 +0100

    update code on borrow checker

--- src/main.rs
+++ src/main.rs
@@ -2,5 +2,19 @@ fn main() {
     let var = 1;
     let mut s = "hello".to_string();
     s.push_str(", world");
-    println!("{var}")
+    println!("{},{}", var, s);
+
+    // let mut str1 = String::from("Tyler");
+    // let mut str2: String;
+
+    // loop {
+    //     str2 = str1;
+    // }
+
+    let mut q = String::from("hey");
+    change_string(&mut q);
+}
+
+fn change_string(greet: &mut String) {
+    greet.push_str(", John");
 }
